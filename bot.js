da432a5a466a5a17ce9fe1440289967eff8c7c74
ozelbot <44919025+ozelbot@users.noreply.github.com>
2018-11-29 18:34:38 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,5 +1,6 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();;
+const economy = require('discord-eco');
 const ayarlar = require('./ayarlar.json');
 const chalk = require('chalk');
 const fs = require('fs');
@@ -12,6 +13,24 @@ const log = message => {
   console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] ${message}`);
 };
 
+client.on('message', message => {
+  
+  if(message.content === "--para") {
+  economy.fetchBalance(message.author.id).then(i => {
+  message.reply("paran: " + i.money + "TL")
+  })
+  }
+  
+  var d1 = Math.floor(Math.random() *7) + 8;
+  var d2 = Math.floor(Math.random() *7) + 8;
+  console.log(d1 + "," + d2)
+  
+  if(d1 === d2) {
+  economy.updateBalance(message.author.id, d1).then(i => {
+    message.reply(d1 + "TL kazandın!")
+  })
+  }
+	///////////////////////////////////////////
 
 client.commands = new Discord.Collection();
 client.aliases = new Discord.Collection();
