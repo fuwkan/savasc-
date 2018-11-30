1ae30095ff764c5bf7b08ee16d321df7df7bf5b8
ozelbot <44919025+ozelbot@users.noreply.github.com>
2018-11-30 14:44:21 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -558,6 +558,16 @@ client.on("guildMemberAdd", member => {
 });
 
 ////////////////////////
+
+client.elevation = message => {
+  if(!message.guild) {
+	return; }
+  let permlvl = 0;
+  if (message.member.hasPermission("BAN_MEMBERS")) permlvl = 2;
+  if (message.member.hasPermission("ADMINISTRATOR")) permlvl = 3;
+  if (message.author.id === ayarlar.sahip) permlvl = 4;
+  return permlvl;
+};
   
 client.on('warn', e => {
   console.log(chalk.bgYellow(e.replace(regToken, 'that was redacted')));
