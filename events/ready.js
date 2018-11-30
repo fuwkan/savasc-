3e4c58e71df6d4dd076dbb3e53822232772af318
ozelbot <44919025+ozelbot@users.noreply.github.com>
2018-11-30 15:25:41 +0300

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -10,4 +10,5 @@ module.exports = client => {
   console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] BOT: ${client.user.username} ismi ile giriş yapıldı!`);
   client.user.setStatus("online");
 client.user.setGame("!yardım | Türkiye | " + client.guilds.size + " Sunucu | " + client.users.size + " Kullanıcı | ");
+client.user.setGame("Bot 7/24 ! Yeni Komutlar ! ");
 }
